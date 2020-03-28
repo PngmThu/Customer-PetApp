84a230105ca8940e83280bacbc0428685683650f
cuongquangnam <tccuong1999@gmail.com>
2020-03-28 17:01:34 +0800

    add Service+Schedule Model+API

---
+++ api/ScheduleAPI.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import Globals from '../globals/globals';
+import AuthAPI from './AuthAPI';
+
+export default class NotificationAPI{
+    constructor() {
+        this.globals = new Globals();
+    }
+
+    async getUnavailableDateByVendorId(vendor){
+        const url = this.globals.serverHost + '/api/schedule/vendorId/'+vendor._id;
+
+        let options = {
+            headers: {token: AuthAPI.retrieveToken(), 'Access-Control-Allow-Origin':'*'}
+        }
+
+        return axios.get(url,options);
+    }
+
+}
