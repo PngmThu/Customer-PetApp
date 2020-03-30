e72f2cb3d40954af32d5648aea1e57ebd93ce28e
cuongquangnam <tccuong1999@gmail.com>
2020-03-30 18:15:09 +0800

    NotificationAPI

--- api/NotificationAPI.js
+++ api/NotificationAPI.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
 import Globals from '../globals/globals';
-
+import AuthAPI from './AuthAPI';
 export default class NotificationAPI{
     constructor() {
         this.globals = new Globals();
+        this.authAPI = new AuthAPI();
     }
 
-    postNotification(token, content, time, customerId, vendorId){
+    postNotification(Notification){
 
         const url = this.globals.serverHost + '/api/notification/add';
 
         let options = {
-            headers: {token: token, 'Access-Control-Allow-Origin':'*'}
+            headers: {token: this.authAPI.retrieveToken(), 'Access-Control-Allow-Origin':'*'}
         };
 
         let body = {content: content, time: time, customerId: customerId, vendorId: vendorId};
@@ -23,21 +24,35 @@ export default class NotificationAPI{
         const url = this.globals.serverHost + '/api/notification/'+id;
 
         let options = {
-            headers: {token: token, 'Access-Control-Allow-Origin':'*'}
+            headers: {token: this.authAPI.retrieveToken(), 'Access-Control-Allow-Origin':'*'}
         };
 
         return axios.get(url, options)
     }
 
-    getNotificationByCustomerId(token,customerId){
+    async getNotificationByCustomer(customer,callback){
 
-        const url = this.globals.serverHost + '/api/notification/customer/'+customerId;
-
+        const url = this.globals.serverHost + '/api/notification/customer/';
+        const token = await this.authAPI.retrieveToken();
         let options = {
-            headers: {token: token, 'Access-Control-Allow-Origin':'*'}
+            headers: {'token':token, 'Access-Control-Allow-Origin':'*'}
         };
+        let body = {customerId: customer._id};
+        console.log(options);
+        await axios.get(url,body,options)
+             .then(res=>{
+                 if (res.status==200){
+                     callback(false,res.data);
+                 }
+                 else {
+                     callback(res.data);
+                 }
+             })
+             .catch(err=>{
+                 callback(err);
+             })
 
-        return axios.get(url, options)
+        console.log('jeljwelrj')
     }
 
     getNotificationByPetId(token,petId){
